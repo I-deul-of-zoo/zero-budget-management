41ada3e5ac4b5809039b1b0beb0e8cf3c5568149
airhac <oooh32141@gmail.com>
2023-11-13 01:09:59 +0900

    :bug:Fix : 버그 수정 #7  - 예산 설정(API)시 unique_together 설정시 데이터 생성은 잘되지만 어떠한 값에대한 update시 {     "error": "중복되는 category가 이미 존재합니다. 해당 내용을 상세페이지에서 확인하고 변경해주세요" } 와 같이 원래 있는 category의 amount를 변경하지 못하는 오류가 발생 좀더 유연한 기능을 제공해주는 UniqueConstraint로 unique로 설정 복합키 대신 사용

--- budgets/models.py
+++ budgets/models.py
@@ -20,4 +20,8 @@ class Budgets(models.Model):
         #unique_together은 두개의 field를 가지고 unique를 적용하기 위해 사용되어 진다
         #다만, 문제점은 post일 경우 해당 데이터 검증이 되지만 update작업이 허용되지 않을 수 있게 됩닏.
         # 그래서, Django doc에서는 해당 기능에대해 유연성과 확장성을 늘리기 위해 UniqueConstraint를 지원합니다.
-        unique_together = ('user', 'category')
+        # unique_together = ('user', 'category')
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'category'], name='unique_user_category'),
+            # 다른 제약 조건들...
+        ]
