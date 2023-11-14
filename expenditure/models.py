299b06119eeac1c0901c62b46c3ae4322998711a
airhac <oooh32141@gmail.com>
2023-11-14 20:54:46 +0900

    :recycle:Refact : 코드 리팩토링 #10 Expenditure에서 ReasonableExpenditure을 참조 할 수 있도록 합니다.

--- expenditure/models.py
+++ expenditure/models.py
@@ -4,17 +4,26 @@ from budgets.models import Category
 User = get_user_model()
 # Create your models here.
 
+
+class ReasonableExpenditure(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    reasonable_amount = models.PositiveIntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    #한 유저와 한 카테고리에 대한 합리적인 가격은 하나여야 합니다.
+    constraints = [
+            models.UniqueConstraint(fields=['user', 'category'], name='unique_user_category_expenditure'),
+            # 다른 제약 조건들...
+        ]
+    
 class Expenditure(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    reasonable = models.ForeignKey(ReasonableExpenditure, on_delete=models.CASCADE)
     consumption_amount = models.PositiveIntegerField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=1000)
     is_except = models.BooleanField(default=False)
     
-class ReasonableExpenditure(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    reasonable_amount = models.PositiveIntegerField(default=0)
-    updated_at = models.DateTimeField(auto_now=True)
