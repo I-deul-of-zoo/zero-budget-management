544fb96250be005bbaba64e46a48ce51ad16c554
airhac <oooh32141@gmail.com>
2023-11-14 20:39:18 +0900

    :recycle:Refact : 코드 리팩토링 #10  - Expenditure의 합리적 금액은 지출마다 생성이 되면 update시 같은 값을    여러번 update하여 비효율적입니다. 그래서 해당 category에 대한    합리적 금액을 한번만 update하기 위해 데이터 베이스 분리

--- expenditure/models.py
+++ expenditure/models.py
@@ -7,9 +7,14 @@ User = get_user_model()
 class Expenditure(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    reasonable_amount = models.PositiveIntegerField(default=0)
     consumption_amount = models.PositiveIntegerField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=1000)
     is_except = models.BooleanField(default=False)
+    
+class ReasonableExpenditure(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    reasonable_amount = models.PositiveIntegerField(default=0)
+    updated_at = models.DateTimeField(auto_now=True)
